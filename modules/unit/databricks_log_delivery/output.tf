57825e1f8f1a4309b7cf8ef0279bae300ec3e286
Rohit Dashora <rohitdashora@gmail.com>
2021-09-22 02:05:54 +0530

    modified output

--- modules/unit/databricks_log_delivery/output.tf
+++ modules/unit/databricks_log_delivery/output.tf
@@ -12,3 +12,19 @@ output "mws_credential_credentials_id" {
   value          = module.databricks_mws_credentials.credentials_id
   description = "(String) identifier of credentials"
 }
+
+// storage config
+output "mws_storage_configuration_id" {
+  value       = module.databricks_storage_configuration.id
+  description = "Canonical unique identifier for the mws storage configurations."
+}
+output "storage_configuration_id" {
+  value       = module.databricks_storage_configuration.storage_configuration_id
+  description = "(String) id of storage config to be used for"
+}
+
+//log config
+output "log_delivery_config_id" {
+  value       = module.databricks_mws_log_delivery.config_id
+  description = "- Databricks log delivery configuration ID."
+}
