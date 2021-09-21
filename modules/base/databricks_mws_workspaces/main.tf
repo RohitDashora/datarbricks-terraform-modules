885f81c47e3437468de2e1462cace7330affa9d7
Rohit Dashora <rohitdashora@Rohits-MacBook-Pro-2.local>
2021-09-21 23:44:19 +0530

    fmt

--- modules/base/databricks_mws_workspaces/main.tf
+++ modules/base/databricks_mws_workspaces/main.tf
@@ -1,14 +1,13 @@
 resource "databricks_mws_workspaces" "this" {
-account_id = var.account_id
-managed_services_customer_managed_key_id = var.managed_services_customer_managed_key_id
-deployment_name = var.deployment_name
-workspace_name = var.workspace_name
-aws_region = var.aws_region
-storage_configuration_id = var.storage_configuration_id
-private_access_settings_id = var.private_access_settings_id
-network_id = var.network_id
-credentials_id = var.credentials_id
-storage_customer_managed_key_id = var.storage_customer_managed_key_id
-
+  account_id                               = var.account_id
+  managed_services_customer_managed_key_id = var.managed_services_customer_managed_key_id
+  deployment_name                          = var.deployment_name
+  workspace_name                           = var.workspace_name
+  aws_region                               = var.aws_region
+  storage_configuration_id                 = var.storage_configuration_id
+  private_access_settings_id               = var.private_access_settings_id
+  network_id                               = var.network_id
+  credentials_id                           = var.credentials_id
+  storage_customer_managed_key_id          = var.storage_customer_managed_key_id
 
 }
--- modules/base/databricks_mws_workspaces/output.tf
+++ modules/base/databricks_mws_workspaces/output.tf
@@ -1,20 +1,20 @@
 output "id" {
-    value= databricks_mws_workspaces.mws_workspace.id
-    description= "Canonical unique identifier for the workspace."
-    }
+  value       = databricks_mws_workspaces.mws_workspace.id
+  description = "Canonical unique identifier for the workspace."
+}
 output "workspace_status_message" {
-    value= databricks_mws_workspaces.mws_workspace.workspace_status_message
-    description= "(String) updates on workspace status"
-    }
+  value       = databricks_mws_workspaces.mws_workspace.workspace_status_message
+  description = "(String) updates on workspace status"
+}
 output "workspace_status" {
-    value= databricks_mws_workspaces.mws_workspace.workspace_status
-    description= "(String) workspace status"
-    }
+  value       = databricks_mws_workspaces.mws_workspace.workspace_status
+  description = "(String) workspace status"
+}
 output "creation_time" {
-    value= databricks_mws_workspaces.mws_workspace.creation_time
-    description= "(Integer) time when workspace was created"
-    }
+  value       = databricks_mws_workspaces.mws_workspace.creation_time
+  description = "(Integer) time when workspace was created"
+}
 output "workspace_url" {
-    value= databricks_mws_workspaces.mws_workspace.workspace_url
-    description= "(String) URL of the workspace"
-    }
+  value       = databricks_mws_workspaces.mws_workspace.workspace_url
+  description = "(String) URL of the workspace"
+}
