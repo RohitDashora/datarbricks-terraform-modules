05fda762e09597d28b41bc741f2a393bfdc6b105
Rohit Dashora <rohitdashora@gmail.com>
2022-12-05 13:46:57 -0500

    better tf state reader

--- scripts/render_terraform_graph.py
+++ scripts/render_terraform_graph.py
@@ -1,28 +1,33 @@
 import json
 import networkx as nx
-
-def render_terraform_graph(state_file_path):
-    # read the Terraform state file
-    with open(state_file_path, "r") as f:
-        state = json.load(f)
-
-    # create a directed graph using NetworkX
-    graph = nx.DiGraph()
-
-    # iterate over the resources in the Terraform state and add them to the graph
-    for resource_type, resources in state["resources"].items():
-        for resource in resources:
-            graph.add_node(resource["name"])
-            for dependency in resource["depends_on"]:
-                graph.add_edge(dependency, resource["name"])
-
-    # use NetworkX to render the graph
-    nx.draw(graph, with_labels=True)
-
+import matplotlib.pyplot as plt
 
 # specify the path to the Terraform state file
-state_file_path = "scripts/render_terraform_graph.py"
+state_file_path = "path/to/terraform.tfstate"
 
-# call the function to render the graph
-render_terraform_graph(state_file_path)
+def read_and_render_terraform_state(state_file_path: str):
+    try:
+        # read the Terraform state file
+        with open(state_file_path, "r") as f:
+            state = json.load(f)
 
+        # check that the state variable is a dictionary
+        if not isinstance(state, dict):
+            raise ValueError("The Terraform state is not a valid JSON file.")
+
+        # create a directed graph using NetworkX
+        graph = nx.DiGraph()
+
+        # iterate over the modules in the Terraform state and add them to the graph
+        for module in state["modules"]:
+            graph.add_node(module["path"][0])
+            for resource in module["resources"].values():
+                graph.add_node(resource["name"])
+                graph.add_edge(module["path"][0], resource["name"])
+
+        # use NetworkX to render and display the graph
+        nx.draw(graph, with_labels=True)
+        plt.show()
+    except Exception as e:
+        # handle any errors that may occur
+        print("Error:", e)
