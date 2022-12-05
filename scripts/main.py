b7d48c10e5e485258d89849304b617bac11b0422
Rohit Dashora <rohitdashora@gmail.com>
2022-12-05 13:24:49 -0500

    changed the structure of tf read graph

---
+++ scripts/main.py
@@ -0,0 +1,5 @@
+# specify the path to the Terraform state file
+state_file_path = "scripts/render_terraform_graph.py"
+
+# call the function to render the graph
+render_terraform_graph(state_file_path)
