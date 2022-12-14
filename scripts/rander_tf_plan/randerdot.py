edbdd0d7d699b84cf74ed4d7c1abdd1e38da3f17
Rohit Dashora <rohitdashora@gmail.com>
2022-12-14 16:09:37 -0500

    Changes to be committed: 	modified:   scripts/rander_tf_plan/randerdot.py

--- scripts/rander_tf_plan/randerdot.py
+++ scripts/rander_tf_plan/randerdot.py
@@ -1,2 +1,38 @@
-import graphviz
-graphviz.Source.from_file('graph4.dot')
+import os
+import sys
+import pygraphviz as pgv
+
+def render_dot_file(dot_file, output_file):
+    try:
+        # Check if the input file exists
+        if not os.path.isfile(dot_file):
+            raise IOError(f'{dot_file} does not exist')
+
+        # Read the DOT file
+        g = pgv.AGraph(dot_file)
+
+        # Check if the output file exists
+        if os.path.isfile(output_file):
+            raise IOError(f'{output_file} already exists')
+
+        # Render the graph
+        g.draw(output_file, format='png')
+
+    except IOError as e:
+        # Handle I/O errors
+        print(f'Error: {e}')
+
+    except pgv.error.Error as e:
+        # Handle errors from pygraphviz
+        print(f'Error: {e}')
+
+# Example usage
+dot_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# If no output file was specified, use the same filename as the input file
+# but with a .png extension
+if not output_file:
+    output_file = os.path.splitext(dot_file)[0] + '.png'
+
+render_dot_file(dot_file, output_file)
